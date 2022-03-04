65f236bcc9bcb921c9f029bece6c79d9f8a0bbc1
ERIC WEBB <eric.b.webb13.civ@mail.mil>
2022-03-04 00:08:09 -0500

    basic outline done.

--- AngularAppTDD/src/app/app-routing.module.ts
+++ AngularAppTDD/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { MintComponent } from './mint/mint.component';
+import { RoadMapComponent } from './road-map/road-map.component';
+import { TeamComponent } from './team/team.component';
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'mint', component: MintComponent },
+  { path: 'roadmap', component: RoadMapComponent },
+  { path: 'team', component: TeamComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
