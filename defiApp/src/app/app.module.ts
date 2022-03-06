34a665ccdfd58dac1ea4cee29b8f25e7aae2b699
ERIC WEBB <eric.b.webb13.civ@mail.mil>
2022-03-06 17:52:31 -0500

    test

--- defiApp/src/app/app.module.ts
+++ defiApp/src/app/app.module.ts
@@ -1,28 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// import Web3 from 'web3';
+// import { ImmutableXClient, Link } from '@imtbl/imx-sdk';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TeamComponent } from './team/team.component';
 import { RoadMapComponent } from './road-map/road-map.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+// import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+// import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+// import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
 import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+// import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+// import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+// import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+// import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+// import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+// import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+// import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+// import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+// import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+// import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+// import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { YourKrowsComponent } from './your-krows/your-krows.component';
 
@@ -38,22 +39,24 @@ import { YourKrowsComponent } from './your-krows/your-krows.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MdbAccordionModule,
-    MdbCarouselModule,
-    MdbCheckboxModule,
+    // ImmutableXClient,
+    // Web3, 
+    // MdbAccordionModule,
+    // MdbCarouselModule,
+    // MdbCheckboxModule,
     MdbCollapseModule,
-    MdbDropdownModule,
-    MdbFormsModule,
-    MdbModalModule,
-    MdbPopoverModule,
-    MdbRadioModule,
-    MdbRangeModule,
-    MdbRippleModule,
-    MdbScrollspyModule,
-    MdbTabsModule,
-    MdbTooltipModule,
-    MdbValidationModule,
-    NoopAnimationsModule
+    // MdbDropdownModule,
+    // MdbFormsModule,
+    // MdbModalModule,
+    // MdbPopoverModule,
+    // MdbRadioModule,
+    // MdbRangeModule,
+    // MdbRippleModule,
+    // MdbScrollspyModule,
+    // MdbTabsModule,
+    // MdbTooltipModule,
+    // MdbValidationModule,
+    // NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- defiApp/src/app/your-krows/your-krows.component.ts
+++ defiApp/src/app/your-krows/your-krows.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+// import { ImmutableXClient, Link } from '@imtbl/imx-sdk';
+// import { ImmutableXClient } from "@imtbl/imx-sdk";
+// import { Link } from '@imtbl/imx-sdk';
+// import ImmutableXClient as ImmutableXClient from '@imtbl/imx-sdk'
+//import * as ImmutableXClient from '@imtbl/imx-sdk'
 
+import Web3 from 'web3';
 @Component({
   selector: 'app-your-krows',
   templateUrl: './your-krows.component.html',
@@ -9,7 +15,18 @@ export class YourKrowsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    // this.link = new Link(this.linkAddress);
+    // this.client = await ImmutableXClient.build({ publicApiUrl: this.apiAddress });
   }
+
+  errorMessage = '';
+  loading = false
+  krowsArray = [];
+  linkAddress = 'https://link.x.immutable.com';
+  apiAddress = 'https://api.x.immutable.com/v1';
+  // link: Link = new Link;
+  // client!: ImmutableXClient;
+
 
 }
